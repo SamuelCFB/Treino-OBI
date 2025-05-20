d89c42a97392b55483d8b4e65e42f393398adb1b
SamuelCFB <147280267+SamuelCFB@users.noreply.github.com>
2025-05-20 15:13:04 -0300

    Update TempoDeResposta.cpp

--- TempoDeResposta.cpp
+++ TempoDeResposta.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
+    // Quantidade de registros
     int N;
-    // registros
+    // Registros
     string Reg;
-    // numero dos amigos de sara
+    // Numeros dos amigos de sara
     vector<int> Am = {};
+    int AmAUX;
 
     scanf("%d", &N);
     for(int i = 0; i < N; ++i){
-        cin >> Reg;
-    }
+        cin >> Reg >> AmAUX;
+        auto it = find(Am.begin(), Am.end(), AmAUX);
+        if(it != Am.end()){
+            Am.push_back(AmAUX);
+        }
+        }
     
     return 0;
 }
