72f787de33d995948a2bb91a15086b3de4991546
SamuelCFB <samuel.cfb102009@gmail.com>
2025-05-20 10:24:31 -0300

    primeiros passo do código:Tempo de resposta

---
+++ TempoDeResposta.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    // registros
+    string Reg;
+    // numero dos amigos de sara
+    vector<int> Am = {};
+
+    scanf("%d", &N);
+    for(int i = 0; i < N; ++i){
+        cin >> Reg;
+    }
+    
+    return 0;
+}
